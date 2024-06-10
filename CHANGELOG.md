91cc7e20ae328d23503856360389cda7a1591a0e
Onur Ozgur OZKAN <onur.ozgur.ozkan@bulutfon.com>
2024-06-10 09:02:11 +0000

    Add MARKDOWN files.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v0.1.0 (2024-06-10)
+
+- Add README.MD, CHANGELOG.md, LICENSE.md
+- Add Dependabot and ci.yml
--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule HipcallSdk.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/hipcall/elixir_sdk"
+  @version "0.5.0"
+
   def project do
     [
       app: :hipcall_sdk,
-      version: "0.1.0",
-      elixir: "~> 1.15",
+      name: "HipcallSdk",
+      description: "Official Hipcall API Wrapper written in Elixir.",
+      version: @version,
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -23,6 +30,28 @@ defmodule HipcallSdk.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Onur Ozgur OZKAN"],
+      licenses: ["MIT"],
+      links: %{
+        "Website" => "https://www.hipcall.com/en-gb/",
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  def docs do
+    [
+      main: "readme",
+      name: "HipcallOpenai",
+      canonical: "https://hex.pm/packages/elixir_sdk",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE.md"]
     ]
   end
 end
